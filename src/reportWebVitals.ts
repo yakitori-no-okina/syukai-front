5bc382ae2a589a151d77aef7dcaaa873a1b5b8d3
tamemoto <tamemotoyou@gmail.com>
2021-03-18 22:18:47 +0900

    fix: update

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,6 +1,6 @@
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
@@ -8,7 +8,8 @@ const reportWebVitals = (onPerfEntry?: ReportHandler) => {
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    }).catch((err) => console.error(err)) // eslint-disable-line no-console
+    ;
   }
 };
 
