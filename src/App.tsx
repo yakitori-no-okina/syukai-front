0dab0fecb0ec3d7ca6be86a815b6835e0f0095b1
tamemoto <tamemotoyou@gmail.com>
2021-03-19 02:16:22 +0900

    fix: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./tailwind.output.css"
+import "./App.css"
 
 
 const App: React.VFC = () => (
-    <div className="bg-red-500">
-      aaaa
-    </div>
+    <Router>
+        <section className="container mx-auto">
+            <Switch>
+                <Route path="/" exact>
+                    hoge
+                </Route>
+                <Route path="/:id/profile">
+                    profile
+                </Route>
+            </Switch>
+        </section>
+    </Router>
 )
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   purge: {
-    enabled: true,
     content: ['./src/**/*.{js,jsx,ts,tsx}']
   },
   darkMode: false, // or 'media' or 'class'
