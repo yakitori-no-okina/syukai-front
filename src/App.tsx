ba5fa6eea96fe373ebd7fd423d684a680468d8f9
tamemoto <tamemotoyou@gmail.com>
2021-03-20 22:26:59 +0900

    fix: fix tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,18 @@ import "./App.css"
 import AppHeader from "./components/layouts/AppHeader";
 import RecruitmentList from "./containers/Recruitments/RecruitmentList";
 import RecruitmentContent from "./containers/Recruitments/RecruitmentDetail";
-import ProfileContent from "./containers/Profile/Profile";
+import Profile from "./containers/Profile";
+import Search from "./containers/Search";
 
 const App: React.VFC = () => (
     <Router>
         <AppHeader/>
         <section className="container mx-auto">
             <Switch>
-                <Route path="/" exact>
-                    home
-                </Route>
-                <Route path="/recruitment" component={RecruitmentList} exact/>
+                <Route path="/" component={RecruitmentList} exact />
                 <Route path="/recruitment/:id" component={RecruitmentContent} />
-                <Route path="/profile/:id" component={ProfileContent} />
+                <Route path="/search" component={Search} exact />
+                <Route path="/profile/:id" component={Profile} />
             </Switch>
         </section>
     </Router>
