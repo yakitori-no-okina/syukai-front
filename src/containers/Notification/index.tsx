e99e5e35ce8d8dee2da2ced8b30f2f14f3c52b6e
tamemoto <tamemotoyou@gmail.com>
2021-03-24 11:17:50 +0900

    fix: update

--- src/containers/Notification/index.tsx
+++ src/containers/Notification/index.tsx
@@ -1,34 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Notify, blankNotify } from "../../services/models/notify";
 
-const Notification: React.VFC = () => (
-    <div className="absolute z-30 right-0">
-        <div className="container px-4 py-2 box-border mx-auto shadow-lg bg-white rounded-lg w-64 h-80 overflow-y-scroll">
-            <ul>
-                <li className="my-3">
-                    <div className="flex">
-                        <img className="inline-block rounded-full w-10 h-10" src="https://placehold.jp/150x150.png" alt="" />
-                        <p className="ml-4 break-all leading-none">aaaaaaaaaaassssssssssaaaaaaaasass</p>
-                    </div>
-                    <div className="text-right">
-                        <button type="button" className="bg-custom-blue-base text-white  font-bold text-xs rounded-lg py-1 px-6">
-                            承認する
-                        </button>
-                    </div>
-                </li>
-                <li className="my-3">
-                    <div className="flex">
-                        <img className="inline-block rounded-full w-10 h-10" src="https://placehold.jp/150x150.png" alt="" />
-                        <p className="ml-4 break-all leading-none">aaaaaaaaaaassssssssssaaaaaaaasass</p>
-                    </div>
-                    <div className="text-right">
-                        <button type="button" className="bg-custom-blue-base text-white  font-bold text-xs rounded-lg py-1 px-6">
-                            承認する
-                        </button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-);
+const Notification: React.VFC = () => {
+  const notifications: Notify[] = blankNotify;
+  return (
+      <div className="absolute z-30 right-0">
+          <div className="container px-4 py-2 box-border mx-auto shadow-lg bg-white rounded-lg w-64 h-80 overflow-y-scroll">
+              <ul>
+                  {notifications.map(notification => (
+                      <li key={notification.id} className="my-3">
+                          <div className="flex">
+                              <img className="inline-block rounded-full w-10 h-10" src="https://placehold.jp/150x150.png" alt="" />
+                              <p className="ml-4 break-all leading-none">aaaaaaaaaaassssssssssaaaaaaaasass</p>
+                          </div>
+                          {notification.approvalWaitId && (
+                              <div className="text-right">
+                                  <button type="button" className="bg-custom-blue-base text-white  font-bold text-xs rounded-lg py-1 px-6">
+                                      承認する
+                                  </button>
+                              </div>
+                          )}
+                          {notification.recruitmentId && (
+                              <div className="text-right">
+                                  <Link
+                                      to={`/recruitment/${notification.recruitmentId}`}
+                                      className="bg-custom-blue-base text-white  font-bold text-xs rounded-lg py-1 px-6"
+                                  >
+                                      詳しくみる
+                                  </Link>
+                              </div>
+                          )}
+                      </li>
+                  ))}
+              </ul>
+          </div>
+      </div>
+  );
+};
 
 export default Notification;
--- src/services/models/notify.ts
+++ src/services/models/notify.ts
@@ -1,10 +1,43 @@
 export type Notify = {
     id: number,
     userId: number,
-    about: number,
+    about: string,
     message: string,
     hasRead: boolean,
-    approvalWaitId: number,
-    recruitmentId: number,
+    approvalWaitId: number | null,
+    recruitmentId: number | null,
     dateTime: string,
 };
+
+export const blankNotify: Notify[] = [
+    {
+        id: 1,
+        userId: 1,
+        about: "aaaaaaaaa",
+        message: "aaaaaaaa",
+        hasRead: false,
+        approvalWaitId: null,
+        recruitmentId: 12,
+        dateTime: "2020-01-01"
+    },
+    {
+        id: 2,
+        userId: 1,
+        about: "aaaaaaaaa",
+        message: "aaaaaaaa",
+        hasRead: true,
+        approvalWaitId: null,
+        recruitmentId: 12,
+        dateTime: "2020-01-01"
+    },
+    {
+        id: 3,
+        userId: 1,
+        about: "aaaaaaaaa",
+        message: "aaaaaaaa",
+        hasRead: false,
+        approvalWaitId: 12,
+        recruitmentId: null,
+        dateTime: "2020-01-01"
+    }
+]
